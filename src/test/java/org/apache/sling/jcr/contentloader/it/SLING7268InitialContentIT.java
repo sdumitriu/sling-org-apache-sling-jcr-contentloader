4e92f2077e43ed48b174c782e879cc5c128aed99
Eric Norman <enorman@apache.org>
2017-11-30 12:36:44 -0800

    SLING-7268 correct indentation

--- src/test/java/org/apache/sling/jcr/contentloader/it/SLING7268InitialContentIT.java
+++ src/test/java/org/apache/sling/jcr/contentloader/it/SLING7268InitialContentIT.java
@@ -54,63 +54,63 @@ import org.osgi.framework.Bundle;
  */
 @RunWith(PaxExam.class)
 public class SLING7268InitialContentIT extends ContentBundleTestBase {
-    
-    protected TinyBundle setupTestBundle(TinyBundle b) throws IOException {
-        b.set(SLING_INITIAL_CONTENT_HEADER, DEFAULT_PATH_IN_BUNDLE + ";path:=" + contentRootPath);
-        addContent(b, DEFAULT_PATH_IN_BUNDLE, "SLING-7268.json");
-        return b;
-    }
-    
-    @Test
-    @Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
-    public void bundleStarted() {
-        final Bundle b = PaxExamUtilities.findBundle(bundleContext, bundleSymbolicName);
-        assertNotNull("Expecting bundle to be found:" + bundleSymbolicName, b);
-        assertEquals("Expecting bundle to be active:" + bundleSymbolicName, Bundle.ACTIVE, b.getState());
-    }
-    
-    @Test
-    @Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
-    public void initialContentInstalled() throws RepositoryException {
-        final String folderPath = contentRootPath + "/SLING-7268"; 
-        assertTrue("Expecting initial content to be installed", session.itemExists(folderPath)); 
-        assertEquals("folder has node type 'sling:Folder'", "sling:Folder", session.getNode(folderPath).getPrimaryNodeType().getName()); 
-    }
 
-    @Test
-    @Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
-    public void userCreated() throws RepositoryException {
-        UserManager userManager = AccessControlUtil.getUserManager(session);
-        Authorizable authorizable = userManager.getAuthorizable("sling7268_user");
-        assertNotNull("Expecting test user to exist", authorizable);
-    }
+	protected TinyBundle setupTestBundle(TinyBundle b) throws IOException {
+		b.set(SLING_INITIAL_CONTENT_HEADER, DEFAULT_PATH_IN_BUNDLE + ";path:=" + contentRootPath);
+		addContent(b, DEFAULT_PATH_IN_BUNDLE, "SLING-7268.json");
+		return b;
+	}
 
-    @Test
-    @Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
-    public void groupCreated() throws RepositoryException {
-        UserManager userManager = AccessControlUtil.getUserManager(session);
-        Authorizable authorizable = userManager.getAuthorizable("sling7268_group");
-        assertNotNull("Expecting test group to exist", authorizable);
-        assertTrue(authorizable instanceof Group);
-        Iterator<Authorizable> members = ((Group)authorizable).getMembers();
-        assertTrue(members.hasNext());
-        Authorizable firstMember = members.next();
-        assertEquals("sling7268_user", firstMember.getID());
-    }
+	@Test
+	@Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
+	public void bundleStarted() {
+		final Bundle b = PaxExamUtilities.findBundle(bundleContext, bundleSymbolicName);
+		assertNotNull("Expecting bundle to be found:" + bundleSymbolicName, b);
+		assertEquals("Expecting bundle to be active:" + bundleSymbolicName, Bundle.ACTIVE, b.getState());
+	}
 
-    @Test
-    @Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
-    public void aceCreated() throws RepositoryException {
-        final String folderPath = contentRootPath + "/SLING-7268"; 
-        assertTrue("Expecting test folder to exist", session.itemExists(folderPath)); 
+	@Test
+	@Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
+	public void initialContentInstalled() throws RepositoryException {
+		final String folderPath = contentRootPath + "/SLING-7268"; 
+		assertTrue("Expecting initial content to be installed", session.itemExists(folderPath)); 
+		assertEquals("folder has node type 'sling:Folder'", "sling:Folder", session.getNode(folderPath).getPrimaryNodeType().getName()); 
+	}
 
-        AccessControlManager accessControlManager = AccessControlUtil.getAccessControlManager(session);
+	@Test
+	@Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
+	public void userCreated() throws RepositoryException {
+		UserManager userManager = AccessControlUtil.getUserManager(session);
+		Authorizable authorizable = userManager.getAuthorizable("sling7268_user");
+		assertNotNull("Expecting test user to exist", authorizable);
+	}
+
+	@Test
+	@Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
+	public void groupCreated() throws RepositoryException {
+		UserManager userManager = AccessControlUtil.getUserManager(session);
+		Authorizable authorizable = userManager.getAuthorizable("sling7268_group");
+		assertNotNull("Expecting test group to exist", authorizable);
+		assertTrue(authorizable instanceof Group);
+		Iterator<Authorizable> members = ((Group)authorizable).getMembers();
+		assertTrue(members.hasNext());
+		Authorizable firstMember = members.next();
+		assertEquals("sling7268_user", firstMember.getID());
+	}
+
+	@Test
+	@Retry(intervalMsec=RETRY_INTERVAL, timeoutMsec=RETRY_TIMEOUT)
+	public void aceCreated() throws RepositoryException {
+		final String folderPath = contentRootPath + "/SLING-7268"; 
+		assertTrue("Expecting test folder to exist", session.itemExists(folderPath)); 
+
+		AccessControlManager accessControlManager = AccessControlUtil.getAccessControlManager(session);
 		AccessControlPolicy[] policies = accessControlManager.getPolicies(folderPath);
-        List<AccessControlEntry> allEntries = new ArrayList<AccessControlEntry>(); 
+		List<AccessControlEntry> allEntries = new ArrayList<AccessControlEntry>(); 
 		for (AccessControlPolicy accessControlPolicy : policies) {
 			if (accessControlPolicy instanceof AccessControlList) {
 				AccessControlEntry[] accessControlEntries = ((AccessControlList)accessControlPolicy).getAccessControlEntries();
-                for (AccessControlEntry accessControlEntry : accessControlEntries) {
+				for (AccessControlEntry accessControlEntry : accessControlEntries) {
 					allEntries.add(accessControlEntry);
 				}
 			}
@@ -120,7 +120,7 @@ public class SLING7268InitialContentIT extends ContentBundleTestBase {
 		for (AccessControlEntry accessControlEntry : allEntries) {
 			aceMap.put(accessControlEntry.getPrincipal().getName(), accessControlEntry);
 		}
-		
+
 		//check ACE for sling7268_user
 		AccessControlEntry testUserAce = aceMap.get("sling7268_user");
 		assertNotNull("Expected ACE for test user", testUserAce);
@@ -157,5 +157,5 @@ public class SLING7268InitialContentIT extends ContentBundleTestBase {
 		assertEquals(1, privileges.length);
 
 		assertEquals("Expecting granted read privilege", "jcr:read", privileges[0].getName());
-    }
+	}
 }
